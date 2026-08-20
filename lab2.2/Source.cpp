ccd384b9ba921d9b1d612da412a7f74bca0e0ee9
alan <agaev186@icloud.com>
2026-08-20 12:15:06 +0300

    - поиск элемента(возвращает индекс первого совпавшего элемента, либо - 1, если совпадений нет); - ввод/вывод в консоль (потоковый); - сортировка элементов(любым алгоритмом) - вставка элемента по индексу. Если индекс некорректный, вернуть false; - удаление элемента по индексу. Если индекс некорректный, вернуть false; - удаление элемента по значению (первое вхождение). Если элемент отсутствует в массиве, вернуть false; - удаление всех элементов с заданным значением; - поиск максимального/минимального элемента; - получение итераторов на начало/конец массива (методы должны называться begin и end. Метод end должен возвращать итератор не на последний элемент, а за позицию после него); - добавление элемента в конец массива (+ и +=); - сложение (конкатенация) с другим массивом (здесь имеется в виду другим объектом нашего класса, а не стандартные массивы) (+ и +=);

--- lab2.2/Source.cpp
+++ lab2.2/Source.cpp
@@ -3,6 +3,15 @@
 #include <random>
 
 template<typename ItemType>
+void Swap(ItemType& a, ItemType& b)
+{
+    ItemType c;
+    c = a;
+    a = b;
+    b = c;
+}
+
+template<typename ItemType>
 class DynamicArray
 {
 public:
@@ -16,7 +25,6 @@ public:
 
     ~DynamicArray(); //деструктор
 
-    bool swapArrays(DynamicArray&); //обмен содержимого с другим массивом(swap)
 
 
     DynamicArray& operator=(const DynamicArray&); //присваивание копированием(= )
@@ -37,9 +45,54 @@ public:
         return arrayData_[index];
     }
 
+    // ввод/вывод в консоль(потоковый)
+    friend std::ostream& operator<<(std::ostream& r, const DynamicArray<ItemType>& s)
+    {
+        for (int curIdx = 0; curIdx < s.arrayLength_; ++curIdx)
+            r << s[curIdx] << " ";
+        return r;
+    }
+
+    friend std::istream& operator>>(std::istream& r, DynamicArray<ItemType>& a)
+    {
+        int length;
+        std::cout << "\nВведите длину массива: ";
+        r >> length;
+
+        delete[] a.arrayData_;
+
+        a.arrayLength_ = length;
+        a.arrayData_ = new ItemType[length];
+
+        std::cout << "\nВведите элементы массива через Enter:\n";
+        for (int i = 0; i < a.arrayLength_; i++)
+            r >> a.arrayData_[i];
+
+        return r;
+    }
+
+
+    bool swapArrays(DynamicArray&); //обмен содержимого с другим массивом(swap)
+
+    void sortArray(); //сортировка элементов(любым алгоритмом)
+
     bool insertAt(const int, const ItemType&); //вставка элемента по индексу.Если индекс некорректный, вернуть false
+    int searchEl(const ItemType&) const; //поиск элемента(возвращает индекс первого совпавшего элемента, либо - 1, если совпадений нет)
+    bool deleteAt(const int); //удаление элемента по индексу.Если индекс некорректный, вернуть false
+    bool deleteEl(const ItemType& element); //удаление элемента по значению (первое вхождение). Если элемент отсутствует в массиве, вернуть false
+    bool deleteAllEl(const ItemType&); //удаление всех элементов с заданным значением
 
+    //поиск максимального / минимального элемента
+    ItemType maxEl() const;
+    ItemType minEl() const;
 
+    //добавление элемента в конец массива (+ и +=);
+    DynamicArray operator+(const ItemType& value) const;
+    DynamicArray& operator+=(const ItemType& value);
+
+    //сложение (конкатенация) с другим массивом (здесь имеется в виду другим объектом нашего класса, а не стандартные массивы) (+ и +=);
+    DynamicArray operator+(const DynamicArray& other) const;
+    DynamicArray& operator+=(const DynamicArray& other);
 
     void add(const ItemType&);
 
@@ -147,6 +200,165 @@ bool DynamicArray<ItemType>::swapArrays(DynamicArray& b)
     return true;
 }
 
+template<typename ItemType> //сортировка элементов(любым алгоритмом)
+void DynamicArray<ItemType>::sortArray()
+{
+    int i, j, f;
+    for (i = 0, f = 1; i < arrayLength_ - 1 && f; i++)
+        for (j = 0, f = 0; j < arrayLength_ - i - 1; j++)
+            if (arrayData_[j] > arrayData_[j + 1])
+            {
+                Swap(arrayData_[j], arrayData_[j + 1]);
+                f = 1;
+            }
+}
+
+//поиск элемента(возвращает индекс первого совпавшего элемента, либо - 1, если совпадений нет)
+template<typename ItemType>
+int DynamicArray<ItemType>::searchEl(const ItemType& element) const
+{
+    for (int i = 0; i < arrayLength_; i++)
+    {
+        if (arrayData_[i] == element)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+template<typename ItemType> //вставка элемента по индексу.Если индекс некорректный, вернуть false
+bool DynamicArray<ItemType>::insertAt(const int index, const ItemType& value)
+{
+    if (index == 0 && arrayLength_ == 0)
+    {
+        arrayData_ = new ItemType[1];
+
+        arrayData_[0] = value;
+
+        ++arrayLength_;
+
+        return true;
+    }
+
+    if (index < 0 || index >= arrayLength_) return false;
+
+    ItemType* tempArrayData = new ItemType[arrayLength_ + 1];
+
+    for (int curIdx = 0; curIdx < index; ++curIdx)
+    {
+        tempArrayData[curIdx] = arrayData_[curIdx];
+    }
+
+    for (int curIdx = index; curIdx < arrayLength_; ++curIdx)
+    {
+        tempArrayData[curIdx + 1] = arrayData_[curIdx];
+    }
+
+    tempArrayData[index] = value;
+
+    ++arrayLength_;
+
+    delete[] arrayData_;
+
+    arrayData_ = tempArrayData;
+
+    return true;
+}
+
+template<typename ItemType> //удаление элемента по индексу. Если индекс некорректный, вернуть false
+bool DynamicArray<ItemType>::deleteAt(const int index)
+{
+    if (index < 0 || index >= arrayLength_) return false;
+
+    ItemType* tempArrayData = new ItemType[arrayLength_ - 1];
+
+    for (int curIdx = 0; curIdx < index; ++curIdx)
+    {
+        tempArrayData[curIdx] = arrayData_[curIdx];
+    }
+
+    for (int curIdx = index; curIdx < arrayLength_ - 1; ++curIdx)
+    {
+        tempArrayData[curIdx] = arrayData_[curIdx + 1];
+    }
+
+    --arrayLength_;
+
+    delete[] arrayData_;
+
+    arrayData_ = tempArrayData;
+
+    return true;
+}
+
+template<typename ItemType> //удаление элемента по значению(первое вхождение).Если элемент отсутствует в массиве, вернуть false
+bool DynamicArray<ItemType>::deleteEl(const ItemType& element)
+{
+    int index = -1;
+
+    for (int i = 0; i < arrayLength_; i++)
+    {
+        if (arrayData_[i] == element)
+        {
+            index = i;
+            break;
+        }
+    }
+    if (index == -1) return false;
+
+    ItemType* tempArrayData = new ItemType[arrayLength_ - 1];
+
+    for (int i = 0; i < index; i++)
+    {
+        tempArrayData[i] = arrayData_[i];
+    }
+
+    for (int i = index; i < arrayLength_ - 1; i++)
+    {
+        tempArrayData[i] = arrayData_[i + 1];
+    }
+
+    arrayLength_--;
+
+    delete[] arrayData_;
+
+    arrayData_ = tempArrayData;
+
+    return true;
+}
+
+template<typename ItemType> //удаление всех элементов с заданным значением
+bool DynamicArray<ItemType>::deleteAllEl(const ItemType& value)
+{
+    if (arrayLength_ == 0) return false;
+
+    int newLength = 0;
+    for (int i = 0; i < arrayLength_; ++i)
+    {
+        if (arrayData_[i] != value)
+            ++newLength;
+    }
+    if (newLength == arrayLength_) return false;
+
+    ItemType* newArray = new ItemType[newLength];
+    int newIndex = 0;
+
+    for (int i = 0; i < arrayLength_; ++i)
+    {
+        if (arrayData_[i] != value)
+        {
+            newArray[newIndex] = arrayData_[i];
+            ++newIndex;
+        }
+    }
+
+    delete[] arrayData_;
+    arrayData_ = newArray;
+    arrayLength_ = newLength;
+
+    return true;
+}
+
 template<typename ItemType> //присваивание копированием(= )
 DynamicArray<ItemType>& DynamicArray<ItemType>::operator=(const DynamicArray<ItemType>& otherArray)
 {
@@ -194,44 +406,98 @@ DynamicArray<ItemType>& DynamicArray<ItemType>::operator=(DynamicArray<ItemType>
     return *this;
 }
 
-template<typename ItemType> //вставка элемента по индексу.Если индекс некорректный, вернуть false
-bool DynamicArray<ItemType>::insertAt(const int index, const ItemType& value)
+//добавление элемента в конец массива(+и += );
+template<typename ItemType>
+DynamicArray<ItemType> DynamicArray<ItemType>::operator+(const ItemType& value) const
 {
-    if (index == 0 && arrayLength_ == 0)
-    {
-        arrayData_ = new ItemType[1];
+    DynamicArray result(arrayLength_ + 1);
 
-        arrayData_[0] = value;
+    for (int i = 0; i < arrayLength_; ++i)
+        result.arrayData_[i] = arrayData_[i];
 
-        ++arrayLength_;
+    result.arrayData_[arrayLength_] = value;
 
-        return true;
-    }
-
-    if (index < 0 || index >= arrayLength_) return false;
-
+    return result;
+}
+template<typename ItemType>
+DynamicArray<ItemType>& DynamicArray<ItemType>::operator+=(const ItemType& value)
+{
     ItemType* tempArrayData = new ItemType[arrayLength_ + 1];
 
-    for (int curIdx = 0; curIdx < index; ++curIdx)
+    for (int index = 0; index < arrayLength_; ++index)
     {
-        tempArrayData[curIdx] = arrayData_[curIdx];
+        tempArrayData[index] = arrayData_[index];
     }
 
-    for (int curIdx = index; curIdx < arrayLength_; ++curIdx)
-    {
-        tempArrayData[curIdx + 1] = arrayData_[curIdx];
-    }
-
-    tempArrayData[index] = value;
-
+    tempArrayData[arrayLength_] = value;
     ++arrayLength_;
 
     delete[] arrayData_;
-
     arrayData_ = tempArrayData;
 
-    return true;
+    return *this;
 }
+
+//сложение(конкатенация) с другим массивом(здесь имеется в виду другим объектом нашего класса, а не стандартные массивы) (+и += );
+template<typename ItemType>
+DynamicArray<ItemType> DynamicArray<ItemType>::operator+(const DynamicArray& other) const
+{
+    if (other.arrayLength_ == 0) return DynamicArray(*this);
+
+    DynamicArray a(arrayLength_ + other.arrayLength_);
+
+    for (int i = 0; i < arrayLength_; ++i)
+        a.arrayData_[i] = arrayData_[i];
+
+    for (int i = 0; i < other.arrayLength_; ++i)
+        a.arrayData_[arrayLength_ + i] = other.arrayData_[i];
+
+    return a;
+}
+template<typename ItemType>
+DynamicArray<ItemType>& DynamicArray<ItemType>::operator+=(const DynamicArray& other)
+{
+    if (other.arrayLength_ == 0) return *this;
+
+    int newLength = arrayLength_ + other.arrayLength_;
+    ItemType* newData = new ItemType[newLength];
+
+    for (int i = 0; i < arrayLength_; ++i)
+        newData[i] = arrayData_[i];
+
+    for (int i = 0; i < other.arrayLength_; ++i)
+        newData[arrayLength_ + i] = other.arrayData_[i];
+
+    delete[] arrayData_;
+    arrayData_ = newData;
+    arrayLength_ = newLength;
+
+    return *this;
+}
+
+//поиск максимального / минимального элемента
+template<typename ItemType>
+ItemType DynamicArray<ItemType>::maxEl() const
+{
+    ItemType max = arrayData_[0];
+    for (int i = 1; i < arrayLength_; i++)
+        if (arrayData_[i] > max)
+            max = arrayData_[i];
+    return max;
+}
+
+// Минимальный элемент
+template<typename ItemType>
+ItemType DynamicArray<ItemType>::minEl() const
+{
+    ItemType min = arrayData_[0];
+    for (int i = 1; i < arrayLength_; i++)
+        if (arrayData_[i] < min)
+            min = arrayData_[i];
+    return min;
+}
+
+
 
 template<typename ItemType>
 void DynamicArray<ItemType>::add(const ItemType& value)
@@ -251,31 +517,21 @@ void DynamicArray<ItemType>::add(const ItemType& value)
     arrayData_ = tempArrayData;
 }
 
+//получение итераторов на начало / конец массива(методы должны называться begin и end.Метод end должен возвращать итератор не на последний элемент, а за позицию после него)
+template<typename ItemType>
+ItemType* begin(const DynamicArray<ItemType>& array)
+{
+    return array.getData();
+}
 
+template<typename ItemType>
+ItemType* end(const DynamicArray<ItemType>& array)
+{
+    return array.getData() + array.getLength();
+}
 
 int main()
 {
-    std::random_device randomDevice;
-    std::mt19937_64 generator(randomDevice());
-    std::uniform_real_distribution<float> distribution(-100.0f, 100.0f);
-
-    DynamicArray<float> floatArray(100);
-
-    for (int i = 0; i < 100; i++)
-    {
-        floatArray[i] = distribution(generator);
-
-        std::cout << floatArray[i] << std::endl;
-    }
-
-    DynamicArray<unsigned int> uintArray(100);
-
-    for (int i = 0; i < 100; i++)
-    {
-        uintArray[i] = distribution(generator) + 100u;
-
-        std::cout << uintArray[i] << std::endl;
-    }
 
     return 0;
 }
